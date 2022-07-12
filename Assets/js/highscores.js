399efad4bca968ab4850216a989d3d0456755236
JC Berry <jcharlesberry@gmail.com>
2022-07-12 16:00:12 -0700

    initial commit of some files and updates to others

--- Assets/js/highscores.js
+++ Assets/js/highscores.js
@@ -1,20 +1,28 @@
 function printHighscores() {
     // either get scores from localstorage or set to empty array
     var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
-  
+    //JSON.parse(window.localStorage.getItem("highscores")) || [];
+    //console.log(window.localStorage.getItem("highscores"));
     // sort highscores by score property in descending order
-    highscores.sort(function(a, b) {
-      return b.score - a.score;
-    });
-  
+    
+    //highscores.sort(function(a, b) {
+    //  return b.score - a.score;
+    //});
+    //console.log(initials);
+    //console.log(score);
+
     highscores.forEach(function(score) {
       // create li tag for each high score
-      var liTag = document.createElement("li");
-      liTag.textContent = score.initials + " - " + score.score;
-  
+      console.log(score.initials + " - " + score.score);
+      var tableEl = document.getElementById("highscores");
       // display on page
-      var olEl = document.getElementById("highscores");
-      olEl.appendChild(liTag);
+      var tdTag = document.createElement("td");
+      tdTag.className = "vpb-3 mb-4 border-bottom";
+      tdTag.textContent = score.initials + " - " + score.score;
+      var trTag = document.createElement("tr");
+      trTag.className = "row";
+      trTag.appendChild(tdTag);
+      tableEl.appendChild(trTag);
     });
   }
   
@@ -27,3 +35,5 @@ function printHighscores() {
   
   // run function when page loads
   printHighscores();
+
+  
--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -3,6 +3,7 @@
 var currQuestionIndex = 0;
 var time = questions.length * 15;
 var timerId;
+var scores = [];
 //var q = 0;
 
 // variables to reference DOM elements
@@ -13,64 +14,81 @@ var submitBtn = document.getElementById("submit");
 var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
+var loginEl = document.getElementById("login");
 
 // sound effects
 var sfxRight = new Audio("assets/sfx/correct.wav");
 var sfxWrong = new Audio("assets/sfx/incorrect.wav");
 
+var initials;
 function startQuiz() {
-    if(initialsEl.value !== ''){
-        JSON.stringify(window.localStorage.setItem("initials", initialsEl.value));
-        timerEl.textContent = '';
-        // hide start screen
-        var startScreenEl = document.getElementById("start-screen");
-        startScreenEl.setAttribute("class", "hide");
+    while( initialsEl.value == '' ){
+      feedback.setAttribute("class"," show");
+      feedbackEl.textContent = 'Please enter your initials before starting';
+      return;
+    }
+    initials = initialsEl.value;
+    feedback.setAttribute("class"," hide");
+    //feedbackEl.textContent = 'Please enter your initials before starting';
+    //console.log('this');
+    //JSON.stringify(window.localStorage.setItem("initials", initialsEl.value));
+    timerEl.textContent = '';
+    // hide start screen
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.setAttribute("class", "hide");
 
-        // un-hide questions section
-        questionsEl.removeAttribute("class");
+    // un-hide questions section
+    questionsEl.removeAttribute("class");
 
-        // start timer
-        timerId = setInterval(clockTick, 1000);
+    if (timerId) {
+      clearInterval(timerId);
+    }
+    // start timer
+    timerId = setInterval(noteSeconds, 1000);
 
-        // show starting time
-        timerEl.textContent = time;
+    // show starting time
+    timerEl.textContent = time;
 
         //console.log(questions.length);
         //if(timerId > 0 || questions.length > 0){
         // console.log(questions);
             //clear choices
             //currQuestionIndex++;
-            getQ(currQuestionIndex);
-    }else{
-        timerEl.textContent = 'Please enter your initials before starting';
-        startQuiz();
-    }
+    loginEl.style.display = 'none';
+    getQ();
     //return;
  // } else {
    // quizEnd();
  // }
 }
 startBtn.addEventListener('click',startQuiz);
+
 var currQuestion;
 var answer;
-function getQ(currQuestIndex) {
-    while (choicesEl.firstChild) {
-        choicesEl.removeChild(choicesEl.firstChild);
-    }
-    var titleEl = document.getElementById("question-title");
-    currQuestion = questions[currQuestIndex];
-    titleEl.textContent = currQuestion.title;
-    answer = currQuestion.answer;
-    //console.log(answer);
-    currQuestion.choices.forEach(function(choice, i) {
-        var myNewOption = document.createElement('button');
-        myNewOption.textContent = i + 1 + ". " + choice;
-        myNewOption.value = choice;
-        choicesEl.appendChild(myNewOption);
-        myNewOption.addEventListener('click',questionClick);
-        currentQuestionIndex = currQuestIndex;
+
+function clearQs(){
+  while (choicesEl.firstChild) {
+    choicesEl.removeChild(choicesEl.firstChild);
+  }
+}
+function getQ() {
+  //console.log(window.localStorage.getItem("initials"));
+  clearQs();
+  var titleEl = document.getElementById("question-title");
+  currQuestion = questions[currQuestionIndex];
+  titleEl.textContent = currQuestion.title;
+  answer = currQuestion.answer;
+  //console.log(answer);
+  currQuestion.choices.forEach(function(choice, i) {
+        var questionOption = document.createElement('button');
+        questionOption.textContent = i + 1 + ". " + choice;
+        questionOption.value = choice;
+        questionOption.className = "btn btn-success";
+        choicesEl.appendChild(questionOption);
+        questionOption.addEventListener('click',clickOnQ);
+        //currentQuestionIndex = currQuestIndex;
         //console.log(this.textContent);
-    });
+  });
     //questions.length = questions.length - 1;
     /**
     var questions = [
@@ -78,54 +96,113 @@ function getQ(currQuestIndex) {
     choices: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts"
     */
-    
 }
+/*else{
+    
+  if( currQuestionIndex < questions.length ){
+    titleEl.textContent = '';
+    feedbackEl.setAttribute('class','show');
+  }*/
 var score = 0;
-function questionClick(e) {
-    //console.log(e.target.value);
-    if(e.target.value === answer && questions.length <= 0){
+function clickOnQ(e) {
+    //console.log(currQuestionIndex);
+    //currQuestionIndex++;
+    // If answer is correct and there are no questions left
+    if(e.target.value === answer){
+      if(currQuestionIndex >= questions.length - 1){
+        clearQs();
         score = score + 10;
-        console.log('Correct! Play again!');
-        feedbackEl.textContent = `Game Over! Your final score is ${score}`;
-        clearInterval(timerId);
+
+        feedbackEl.setAttribute('class', 'show');
+        feedbackEl.textContent = `Correct! Your final score is ${score}`;
         endQuiz();
-    }else if(e.target.value === answer){
+        //questions.length = questions.length - 1;
+        
+        // If the answer is correct and there are more questions
+        e.target.removeEventListener('click',clickOnQ);
+      }else{
+        //console.log('answer correct and still questions left');
+        currQuestionIndex++;
         score = score + 10;
-        //console.log('Correct!');
-        feedbackEl.textContent = `Correct! Score so far: ${score}`;
-        getQ(currentQuestionIndex + 1);
-    }else if(e.target.value !== answer && questions.length > 0){
-        console.log('Wrong! 10 second penalty.');
-        time = time - 10;
-        getQ(currentQuestionIndex + 1);
-    }else if(e.target.value !== answer && questions.length <= 0){
-        console.log('Wrong!');
-        feedbackEl.textContent = 'Game Over!';
-        clearInterval(timerId);
-        endQuiz();
+        feedbackEl.setAttribute('class', 'show');
+        feedbackEl.textContent = `Correct! Your score is ${score}`;
+        //questions.pop();
+        if(currQuestionIndex < questions.length){
+          getQ();
+        }else{
+          endQuiz();
+        }
+        //questions.length = questions.length - 1;
+      }
+    //if the answer is wrong and there are questions left
+    }else if(e.target.value !== answer){
+        //console.log(currQuestionIndex);
+        if(currQuestionIndex < questions.length-1){
+          //console.log('penalty');
+          feedbackEl.setAttribute('class', 'show');
+          feedbackEl.textContent = `Wrong! 10 second penalty. Your score is ${score}`;
+          time = time - 10;
+          //score = score - 10;
+          //questions.pop();
+          currQuestionIndex++;
+          getQ();
+        //if the answer is wrong and there are no questions left
+        }else{
+          clearQs();
+          //score = score - 10;
+          feedbackEl.setAttribute('class', 'show');
+          feedbackEl.textContent = `Wrong! Your final score is ${score}`;
+          e.target.removeEventListener('click',clickOnQ);
+          endQuiz();
+        }
+    //anything else
     }else{
         console.log('end game');
     }
 }
+var highscores = [];
 
 function endQuiz() {
-    //time = 0;
+    submitBtn.style.display = 'block';
     clearInterval(timerId);
-    timerEl.textContent = 'Game Over';
-    //console.log('Game over');
-    JSON.stringify(window.localStorage.setItem("score", initialsEl.value));
-}
-
-function clockTick() {
-  time--;
-  timerEl.textContent = time;
-  
-  // check if user ran out of time
-  if (time <= 0) {
-    endQuiz();
     time = 0;
-    feedbackEl.textContent = 'Time up! Game Over!';
-  }
+    //console.log(timerId);
+    //timerEl.textContent = 'Game Over';
+    //console.log('Game over');
+    //questionsEl.setAttribute("class", "hide");
+      // show final score
+    timerEl.textContent = time;
+    var newScoreObj = {
+      score: score,
+      initials: initials
+    };
+    //console.log(score, initials);
+    highscores.push(newScoreObj);
+    /*presentItem = 0;
+    function getValue(item) {
+      presentItem += item;
+      console.log(presentItem += item);
+    }
+    highscores.forEach(getValue);*/
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+    document.getElementById("question-title").textContent = "Game Over!";
+    clearQs();
+}
+function noteSeconds() {
+  //if(timerId){
+    //console.log('test');
+    time--;
+    //console.log(time);
+    //console.log(time + " 2");
+    timerEl.textContent = time;
+  //} else {
+    // check if user ran out of time
+    if (time <= 0) {
+      feedbackEl.textContent = 'Times up! Game Over!';
+      timerEl.textContent = 0;
+      endQuiz();
+    }
+  //}
 }
 
 function saveHighscore() {
@@ -133,30 +210,32 @@ function saveHighscore() {
   var initials = initialsEl.value.trim();
 
   // make sure value wasn't empty
-  if (initials !== "") {
+  //if (initials !== "") {
     // get saved scores from localstorage, or if not any, set to empty array
-    var highscores =
-      JSON.parse(window.localStorage.getItem("highscores")) || [];
+    //var highscores =
+      //JSON.parse(window.localStorage.getItem("highscores")) || [];
 
-    // format new score object for current user
+   /* // format new score object for current user
     var newScore = {
       score: time,
       initials: initials
     };
 
     // save to localstorage
-    highscores.push(newScore);
+    highscores.push(newScore);*/
     window.localStorage.setItem("highscores", JSON.stringify(highscores));
 
     // redirect to next page
     window.location.href = "highscores.html";
-  }
+  //}
 }
 
+//getStoredValues();
 function checkForEnter(event) {
 }
 
 // user clicks button to submit initials
+submitBtn.style.display = 'none';
 submitBtn.onclick = saveHighscore;
 
 // user clicks button to start quiz
@@ -164,3 +243,8 @@ startBtn.onclick = startQuiz;
 
 initialsEl.onkeyup = checkForEnter;
 
+/*function getStoredValues(){
+  console.log(window.localStorage.getItem("highscores"));
+  //console.log(window.localStorage.getItem("initials"));
+  //console.log(window.localStorage.getItem("score"));
+}*/
